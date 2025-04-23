9b072933318cc31c518a9e00449fe9f58a215ec1
Bismark <agyarebismark6@gmail.com>
2025-04-23 14:43:30 +0000

    Refactor import statements in Home component for consistent path usage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
-import Benefits from '../components/Benefits';
-import FeaturedProducts from '../components/FeaturedProducts';
-import DiscountedProducts from '../components/DiscountedProducts';
-import WeeklyOffers from '../components/WeeklyOffers';
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Benefits from "@/components/Benefits";
+import FeaturedProducts from "@/components/FeaturedProducts";
+import DiscountedProducts from "@/components/DiscountedProducts";
+import WeeklyOffers from "@/components/WeeklyOffers";
 
 const Home = () => {
   return (
