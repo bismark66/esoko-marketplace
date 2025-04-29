21c00bf6f1b7eea66809612b46b6d82fe8eaf3ce
Bismark <agyarebismark6@gmail.com>
2025-04-29 14:40:14 +0000

    feat: Update User interface and add new types for authentication and error handling

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,8 +24,81 @@ export interface CartItem extends Product {
 
 export interface User {
   id: string;
-  name: string;
+  // name: string;
+  firstName: string;
+  lastName: string;
+  password: string;
   email: string;
-  role: 'farmer' | 'wholesaler';
-  location: string;
+  invitationToken?: string;
 }
+
+export interface RegisterUserResponse {
+  userId?: number;
+  email?: string;
+  userType?: string;
+  status?: string;
+  isEmailVerified?: boolean;
+  statusCode?: number;
+  success?: boolean;
+  msg?: string;
+  data?: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    isGoogle: boolean | null;
+  };
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+  deviceId?: string;
+  deviceInfo?: {
+    appVersion?: string;
+    platform?: string;
+    model?: string;
+  };
+}
+
+export interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: string;
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    userType: "AGENT" | "ADMIN";
+    status: "ACTIVE" | "INACTIVE";
+    lastLogin: string;
+  };
+}
+
+export type PasswordResetResponse = {
+  message: string;
+  status: string;
+};
+
+export type PasswordChangeType = {
+  currentPassword: string;
+  newPassword: string;
+};
+
+export type EmailVerifyType = {
+  message: string;
+  isEmailVerified: boolean;
+};
+
+export type ApiErrorDetail = {
+  [key: string]: any; // Flexible structure for additional details
+};
+
+export type ApiError = {
+  error: {
+    code: string;
+    message: string;
+  };
+  details?: ApiErrorDetail;
+};
