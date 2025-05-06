5e60d29eb273c0e7ec3732c4d9b6679f1ac60034
Bismark <agyarebismark6@gmail.com>
2025-05-06 00:19:14 +0000

    fix: Restore API_BASE_URL to use environment variable for better configuration

--- src/http/httpInstance.ts
+++ src/http/httpInstance.ts
@@ -1,10 +1,8 @@
-// utils/http/httpInstance.ts
 import { getAccessToken } from "../utils/helpers";
 import { handleHttpResponse } from "./httpResponse";
 
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
-const API_BASE_URL ="https://7b49-154-161-17-27.ngrok-free.app/v1"
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 class HttpClient {
   private async request<T, B = unknown>(
     path: string,
