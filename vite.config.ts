7a8b51a1dae0ed3715702ab98464ecf2439adaf4
Bismark <agyarebismark6@gmail.com>
2025-05-06 00:13:00 +0000

    chore: Add Vite plugins for BrowserSync and full reload to enhance development workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
+import VitePluginBrowserSync from "vite-plugin-browser-sync";
+import FullReload from "vite-plugin-full-reload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginBrowserSync(), FullReload(["src/**/*"])], // watch all files in src],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
